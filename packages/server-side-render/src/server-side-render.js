76e631b703686113048ea7b8148d9f10aea1791e
Staci Cooper <staci.cooper@automattic.com>
2021-10-13 15:48:09 -0700

    Remove unnecessary attribute sanitization in ServerSideRender component
    This was previously added because the endpoint called by this component
    validates attributes, which would break with the previous implementation of
    `__internalWidgetId` (since it wasn't a registered attribute).
    
    With the new implementation, `__internalWidgetId` is a valid attribute and
    it's no longer necessary to sanitize here.

--- packages/server-side-render/src/server-side-render.js
+++ packages/server-side-render/src/server-side-render.js
@@ -12,7 +12,6 @@ import { __, sprintf } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
 import { Placeholder, Spinner } from '@wordpress/components';
-import { __experimentalSanitizeBlockAttributes } from '@wordpress/blocks';
 
 export function rendererPath( block, attributes = null, urlQueryArgs = {} ) {
 	return addQueryArgs( `/wp/v2/block-renderer/${ block }`, {
@@ -88,19 +87,15 @@ export default function ServerSideRender( props ) {
 
 		setIsLoading( true );
 
-		const sanitizedAttributes =
-			attributes &&
-			__experimentalSanitizeBlockAttributes( block, attributes );
-
 		// If httpMethod is 'POST', send the attributes in the request body instead of the URL.
 		// This allows sending a larger attributes object than in a GET request, where the attributes are in the URL.
 		const isPostRequest = 'POST' === httpMethod;
 		const urlAttributes = isPostRequest
 			? null
-			: sanitizedAttributes ?? null;
+			: attributes ?? null;
 		const path = rendererPath( block, urlAttributes, urlQueryArgs );
 		const data = isPostRequest
-			? { attributes: sanitizedAttributes ?? null }
+			? { attributes: attributes ?? null }
 			: null;
 
 		// Store the latest fetch request so that when we process it, we can
